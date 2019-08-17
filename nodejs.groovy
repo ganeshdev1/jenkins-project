46114956cd50f69861400ccc27e05e617299b937
ganeshdev1 <45791953+ganeshdev1@users.noreply.github.com>
2019-08-17 19:02:01 +0530

    Update nodejs.groovy

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     	scm {
-        	git('git://github.com/gvenkat09/nodejs-app.git') {  node -> // is hudson.plugins.git.GitSCM
+        	git('https://github.com/ganeshdev1/nodejs-app.git') {  node -> // is hudson.plugins.git.GitSCM
             	node / gitConfigName('DSL User')
             	node / gitConfigEmail('jenkins-dsl@gmail.com')
         	}
